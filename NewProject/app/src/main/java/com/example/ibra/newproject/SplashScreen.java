2641cf601eeefa792b715968cf21ab8f0e3b9922
IbraDirir <khaliildirir@gmail.com>
2015-12-02 16:53:15 +0300

    Splash screen

---
+++ NewProject/app/src/main/java/com/example/ibra/newproject/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.example.ibra.newproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ibra on 12/2/15.
+ */
+public class SplashScreen  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Thread mythread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+
+                    startActivity(i);
+
+                    finish();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mythread.start();
+    }
+}
