20da7ea47ad1938f460e4ce8522a17574dbfe896
Luciekimotho <luciekimotho@gmail.com>
2015-12-03 19:05:59 +0300

    Badge authentification working

--- Mpolice/app/src/main/java/com/example/ibra/newproject/LogIn.java
+++ Mpolice/app/src/main/java/com/example/ibra/newproject/LogIn.java
@@ -2,10 +2,7 @@ package com.example.ibra.newproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +10,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -36,8 +35,10 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener{
     TextView request;
 
     String badgeNumber;
+    String badgeList;
 
     List<ParseObject> obj;
+    List<String> badgeNumbersList=  new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,17 +53,34 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         badgeNumber = et_badge.getText().toString();
+        Log.e("Login", "Edit text number: " + badgeNumber);
+
         ParseQuery<ParseObject> query = ParseQuery.getQuery("_User");
-        query.whereEqualTo("BadgeNumber",badgeNumber );
-        try {
-            obj = query.find();
-            Intent i = new Intent(LogIn.this,MainActivity.class);
-            startActivity(i);
-        }catch (ParseException e){
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
-            Toast.makeText(getBaseContext(), "Invalid badge number", Toast.LENGTH_SHORT);
+        query.whereEqualTo("BadgeNumber", badgeNumber);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject parseObject, ParseException e) {
+                if (e == null) {
+                    Intent i = new Intent(LogIn.this, MainActivity.class);
+                    startActivity(i);
+
+                    int randomPIN = (int) ((Math.random() * 9000) + 1000);
+                    String PINString = String.valueOf(randomPIN);
+
+                    Log.d("Login", "Pin is: " + PINString);
+
+                } else {
+                    if (e.getCode() == ParseException.OBJECT_NOT_FOUND) {
+                        Toast.makeText(getBaseContext(), "Invalid badge number", Toast.LENGTH_SHORT).show();
+                        Log.e("Login", "error obj not found");
+                    } else {
+                        //unknown error, debug
+                    }
+
+                }
+            }
+        });
         }
-        
+
     }
-}
+
