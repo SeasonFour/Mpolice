9f0eb26e0b09124c689a1e4fb5a725768d9bb11e
Luciekimotho <luciekimotho@gmail.com>
2015-12-04 16:23:20 +0300

    App config file to store public variables --parse variables for now

---
+++ Mpolice/app/src/main/java/com/example/ibra/newproject/AppConfig.java
@@ -0,0 +1,11 @@
+package com.example.ibra.newproject;
+
+/**
+ * Created by lucie on 12/4/15.
+ */
+public class AppConfig {
+    public static final String PARSE_CHANNEL = "GET PIN";
+    public static final int NOTIFICATION_ID = 1000;
+    public static final String PARSE_APPLICATION_ID ="7uE5i8t1LBqdC3f5lmlsuCOaXgrZR4iBDtHXtVCO";
+    public static final String PARSE_CLIENT_KEY ="Uqp5HP8zWmKJB8S5N6EzhDzdL8rW6EVZ9emkEAeP";
+}
