eedc430f47876f4b172f04b4864ef82b39043731
Luciekimotho <luciekimotho@gmail.com>
2015-12-08 19:04:39 +0300

    Reporting crime activity able to save items to the database

--- Mpolice/app/src/main/java/com/example/ibra/newproject/Report.java
+++ Mpolice/app/src/main/java/com/example/ibra/newproject/Report.java
@@ -7,8 +7,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class Report extends AppCompatActivity {
+    EditText etViolationOther,etNumberPlate, etDescription,etLocation,etTime;
+    RadioGroup violationType;
+
+    String Violation,Number_Plate, Description, Location, Time;
+
+    ParseObject obj = new ParseObject("Violations");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +32,50 @@ public class Report extends AppCompatActivity {
         toolbar.setTitle("Report a violation");
         setSupportActionBar(toolbar);
 
+        etViolationOther = (EditText) findViewById(R.id.violationOther);
+        etNumberPlate = (EditText) findViewById(R.id.numberPlate);
+        etDescription = (EditText) findViewById(R.id.description);
+        etLocation = (EditText) findViewById(R.id.location);
+        etTime = (EditText) findViewById(R.id.time);
 
     }
 
-    public void type_of_violation(View v){
+    public void typeOfViolation(View v){
         /*Intent i = new Intent(Report.this, MainActivity.class);
         startActivity(i);*/
     }
+
+    public void reportViolation(View v){
+        Violation = etViolationOther.getText().toString().trim();
+        Number_Plate = etNumberPlate.getText().toString().trim();
+        Description = etDescription.getText().toString();
+        Location = etLocation.getText().toString();
+        Time = etTime.getText().toString();
+
+        obj.put("Violation", Violation);
+        obj.put("Number_plate", Number_Plate);
+        obj.put("Description", Description);
+        obj.put("Location", Location);
+        obj.put("Time", Time);
+        obj.saveEventually();
+
+        obj.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    Toast.makeText(getBaseContext(),
+                            "Reporting violation successful",
+                            Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(i);
+                }else {
+                    Toast.makeText(getBaseContext(),
+                            "Error.Reporting violation up not successful",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 
 }
